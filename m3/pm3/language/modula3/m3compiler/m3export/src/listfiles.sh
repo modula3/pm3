6d30054fb733e22981d0d68f3ca7adec368a7347
dagenais <dagenais>
1998-04-28 20:53:35 +0000

    *** empty log message ***

--- m3/pm3/language/modula3/m3compiler/m3export/src/listfiles.sh
+++ m3/pm3/language/modula3/m3compiler/m3export/src/listfiles.sh
@@ -4,5 +4,5 @@
 # $2: current prefix for files
 # $3: desired prefix for files
 #
-grep install_file $1 | sed -e "s+install_file(\"\([^\"]*\)\",[ ]*\"$2\([^\"]*\)\".*+/\1 $3\2+" | sed -e "s+^.*/\([^/]*\) \(.*\)+\2/\1+" | sort | uniq
+grep install_file $1 | sed -e "s+install_file(\"\([^\"]*\)\",[ ]*\"$2\([^\"]*\)\".*+/\1 \2+" | sed -e "s+^.*/\([^/]*\) \(.*\)+$3\2/\1+" | sort | uniq
 grep link_file $1 | sed -e "s+link_file(\"\([^\"]*\)\",[ ]*\"$2\([^\"]*\)\".*+$3\2+"
