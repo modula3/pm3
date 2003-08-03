82182f0781b3a39ada3452b8147f9b9a9a03e979
Henning Thielemann <modula3@henning-thielemann.de>
2003-08-03 23:12:58 +0000

    the preprocessing now catches every '$' sign

--- language/parsing/m3coco/test/f2i3/src/f2i3.sh
+++ language/parsing/m3coco/test/f2i3/src/f2i3.sh
@@ -4,13 +4,14 @@
 # for converting using f2i3
 
 file=$1
-tmp=/tmp/tmp.f
+tmpa=/tmp/tmpa.f
+tmpb=/tmp/tmpb.f
+tmpc=/tmp/tmpc.f
 
 #  this replaces newlines by newlines
 #sed "N
 #s/\n/\\
-#/
-#" $file
+#/" $file
 
 # FORTRAN uses '$' to mark the beginning of a line
 # that is a continuation of the preceding one.
@@ -23,32 +24,33 @@ tmp=/tmp/tmp.f
 # but possible:
 # 'sed' allows for joining two successive lines implicitly,
 # that is 0,1->0; 2,3->2; 4,5->3
-# In a first step we can process all continued even lines (first 'sed')
+# Any deterministic algorithm I tried has its pitfalls.
+# Thus in a first step we can process all continued even lines (first 'sed')
 # then we prepend a blank line ('echo') and
 # and process the result again with 'sed' which now operates on the
 # lines that had odd numbers before.
-# The last step is to remove all blank lines using inverse 'grep' for blank lines.
-# Caution: In the first 'sed' invokation
-# we cannot remove the line feed completely
-# since we would damage the parity.
-# Instead we insert a blank line after each joined one.
-(echo;
-sed -e N -e "s/\n *[$]\(.*\)/\1\\
-/" $file) | \
-sed -e N -e "s/\n *[$]/\1/" | \
-grep -v "^$" >$tmp
+# If the file don't change by this procedure
+# we assume that all '$' are removed.
+# Otherwise we repeat.
+cp $file $tmpa
+while true; do \
+(echo ; sed -e N -e "s/\n *[$]//" $tmpa) > $tmpb ; \
+sed -e N -e "s/\n *[$]//" $tmpb | grep -v "^$" > $tmpc ; \
+cmp -s $tmpa $tmpc && break ; \
+swap=$tmpa; tmpa=$tmpc; tmpc=$swap; \
+done
 
 # throw away all comments that does not contain information
 # that is automatically retrievable
-perl -i -p -e "s/(\* +\w+ +\(.*?(input|output|workspace).*?\)).*(\n)|(\*.*\n)|(.*\n)/\1\3\5/" $tmp
+perl -i -p -e "s/(\* +\w+ +\((input|output|workspace).*?\)).*(\n)|(\*.*\n)|(.*\n)/\1\3\5/" $tmpa
 
 # the last usable comment marks the end of the interface definitions
 # find its line number
 #    grep - list all comments with line numbers
 #    tail - choose the last line
 #    perl - extract the line number
-num=`grep -n "^*" $tmp | tail -n 1 | perl -p -e "s/(\d+).*/\1/"`
+num=`grep -n "^*" $tmpa | tail -n 1 | perl -p -e "s/(\d+).*/\1/"`
 
 # we will throw away the lines below
 # f2i3 needs an extra character for grateful exit
-(head -n $num $tmp;echo) | f2i3 2>/dev/null
+(head -n $num $tmpa;echo) | f2i3 #2>/dev/null
